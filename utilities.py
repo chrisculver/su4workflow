e8c5cbc0fc7588acb588324a65974f230822d744
chrisculver <chris.cb.culver@gmail.com>
2022-08-30 12:37:37 +0100

    conversion to graph looks good

--- utilities.py
+++ utilities.py
@@ -20,3 +20,26 @@ def find_all_baryon_tensors(laphDiagrams):
                 idx+=1
     return allBaryonTensors
 
+
+def diagram_as_graph(d, allBaryonTensors):
+    tst=copy.deepcopy(d)
+
+    graph = {}
+
+    for b in reversed(tst.commuting):
+        tst.commuting.remove(b)
+        bIdx=allBaryonTensors[b.id()]
+    
+        for idx in reversed(b.indices):
+            for e in tst.commuting:
+                if idx in e.indices:
+                    fIdx=allBaryonTensors[e.id()]
+                    contraction=str(bIdx)+str(fIdx)
+                    if contraction in graph:
+                        graph[contraction].append([b.indices.index(idx),e.indices.index(idx)])
+                    else:
+                        graph[contraction]=[[b.indices.index(idx),e.indices.index(idx)]]
+                    
+                    break
+
+    return graph
